34c4f65de291e5cfd073050162f9904a3318e96e
Simone Stefani <s.stefani95@gmail.com>
2018-02-12 17:07:17 +0100

    Setup TravisCI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MerkleTreeElixir.MixProject do
     [
       app: :merkle_tree_elixir,
       version: "0.1.0",
-      elixir: "~> 1.7-dev",
+      elixir: "~> 1.6.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
